d54d01dc7a735c9e74376beda31177f495cedd8b
trantrongnghia-19130151 <19130151@st.hcmuaf.edu.vn>
2022-06-18 23:20:35 +0700

    first commit

---
+++ src/main/java/com/bansach/BanSachOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.bansach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanSachOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanSachOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bansach/BanSachOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bansach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanSachOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
