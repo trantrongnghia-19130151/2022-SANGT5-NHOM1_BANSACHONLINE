a26b5c3f904870af6ea4ef96a28ef4dce11f7c37
trantrongnghia-19130151 <19130151@st.hcmuaf.edu.vn>
2022-06-19 15:36:44 +0700

    fix config

--- src/main/java/com/bansach/BanSachOnlineApplication.java
+++ src/main/java/com/bansach/BanSachOnlineApplication.java
@@ -2,10 +2,17 @@ package com.bansach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.bansach.BanSachOnlineApplication;
 
 @SpringBootApplication
-public class BanSachOnlineApplication {
-
+public class BanSachOnlineApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BanSachOnlineApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BanSachOnlineApplication.class, args);
 	}
