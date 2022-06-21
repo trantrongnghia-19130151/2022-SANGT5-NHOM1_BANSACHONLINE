d07c7fbcd9d85842d039813a57aed316279a9c5d
17130068 <17130068@st.hcmuaf.edu.vn>
2022-06-21 23:13:58 +0700

    filterByPrice

---
+++ src/main/java/com/bansach/controller/RestController.java
@@ -0,0 +1,61 @@
+package com.bansach.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bansach.entities.*;
+import com.bansach.service.BookStoreService;
+
+
+@CrossOrigin("*")
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	@Autowired
+	private BookStoreService bookService; 
+	
+	@GetMapping("/listBook")
+	public List<Book> homeService(){
+		return bookService.listBook();
+	}
+	
+	@GetMapping("/getBook/{id}")
+	public Optional<Book> getById(@PathVariable("id") int id){
+		return bookService.getBookById(id);
+	}
+	
+	@GetMapping("/pagination")
+	public Page<Book> pagination(int pageNumber){
+		return bookService.pagination(pageNumber);
+	}
+	
+	@GetMapping("/filterPrice")
+	public List<Book> filterPrice(double price1, double price2){
+		return bookService.filterPrice(price1, price2);
+	}
+	
+	@GetMapping("/autoComplete")
+	public List<Book> autoComplete( String name){
+		System.out.println("Name : "+name);
+		return bookService.autoComplete(name);
+	}
+	
+	@GetMapping("/quickview")
+	public Book quickView(int id) {
+		System.out.println("Book Id : "+id);
+		return bookService.findBookById(id);
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/bansach/repository/BookRepository.java
@@ -0,0 +1,27 @@
+package com.bansach.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bansach.entities.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+	@Query("SELECT b FROM Book b WHERE b.price BETWEEN  ?1 AND ?2")
+	List<Book> findByPrice(double price1, double price2);
+
+	@Query("SELECT b FROM Book b WHERE b.name like %?1%")
+	List<Book> autoComplete(String name);
+
+	@Query("SELECT b FROM Book b WHERE b.id = ?1")
+	Book findBookById(int id);
+
+	@Query("SELECT b FROM Book b WHERE " + "CONCAT(b.id, ' ', b.name, ' ', b.category, ' ', b.price)" + " LIKE %?1%")
+	public Page<Book> findAll(String keyword, Pageable pageable);
+}
